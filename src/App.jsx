a0d0ddff565c5dc27945cb9dc8a2a1bcd30940dd
Reno-mathews <renomathews10pro@gmail.com>
2025-12-19 11:36:03 +0530

    Initial React Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>My To-Do List</h1>
+
+      <input type="text" placeholder="Enter a task" />
+
+      <button>Add</button>
+    </div>
+  );
+}
+export default App;
