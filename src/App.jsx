e1f0075285c6d59352e1b8ce68f0d0439ebfef59
Reno-mathews <renomathews10pro@gmail.com>
2025-12-19 11:58:07 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,35 @@ function App() {
   const [task, setTask] = useState("");
   const [tasks, setTasks] = useState([]);
 
+  function addTask() {
+  if (task.trim() === "") return;
+
+  setTasks([...tasks, task]);
+  setTask("");
+  }
+
+
 
   return (
     <div>
       <h1>My To-Do List</h1>
 
       <input 
-      type="text" 
-      value={task}
-      onChange= {(e) => setTask(e.target.value)}
-      placeholder="Enter a task" />
+        type="text" 
+        value={task}
+        onChange= {(e) => setTask(e.target.value)}
+        placeholder="Enter a task" 
+        />
 
       <button onClick={addTask}>Add</button>
 
       <ul>
-        
+        {tasks.map((t, index)=> (
+          <li key={index}>{t}</li>
+        ))}
       </ul>
     </div>
   );
 }
 export default App;
 
-function addTask() {
-  if (task.trim() === "") return;
-
-  setTasks([...tasks, task]);
-  setTask("");
-}
