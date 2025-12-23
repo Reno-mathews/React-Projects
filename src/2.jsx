7ac07cc74e33b3663d69df4934a2d2004fca615f
Reno-mathews <renomathews10pro@gmail.com>
2025-12-23 14:01:46 +0530

    Update

--- src/2.jsx
+++ src/2.jsx
@@ -2,4 +2,27 @@ import { useState } from "react";
 
 function App() {
     const [task, setTask] = useState("");
+    const [tasks, setTasks] = useState([]);
+
+    function addTask() {
+        if (task.trim() === "") return;
+
+        setTasks([...tasks, task]);
+        setTask("");
+    }
+
+    return (
+        <div>
+            <h1>My To-Do List</h1>
+
+            <input
+            type="text"
+            value={task}
+            onChange= {(e) => setTask(e.target.value)}
+            placeholder="Enter a Task"
+            />
+
+            <button onClick={addTask}>Add</button>
+        </div>
+    );
 }
